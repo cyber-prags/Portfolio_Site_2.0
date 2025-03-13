cc1ba5b7ae9c839bb985f254f0230ece017a93cd
Pragyan Jyoti Dutta <74003758+cyber-prags@users.noreply.github.com>
2025-03-13 13:11:16 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,24 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pragyan's Portfolio",
-  description: "Modern & Minimal Pragyan's Portfolio",
+  description: "A look into my work and career",
+  openGraph: {
+    title: "Pragyan's Portfolio",
+    description: "A look into my work and career",
+    images: [
+      {
+        url: "/humbnail.png", // Replace with your new thumbnail image
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Pragyan's Portfolio",
+    description: "A look into my work and career",
+    images: ["/thumbnail.png"],
+  },
 };
 
 export default function RootLayout({

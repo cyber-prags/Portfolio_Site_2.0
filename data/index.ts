7681fc3384fc07e6b2b20b8e235183b99b2bbdb3
Pragyan Jyoti Dutta <74003758+cyber-prags@users.noreply.github.com>
2025-08-13 22:31:24 +0100

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -50,7 +50,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently working on creating an Agentic Workflow for an automated Travel App",
+    title: "Currently working on building the next generation of Product Analytics Platform",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
